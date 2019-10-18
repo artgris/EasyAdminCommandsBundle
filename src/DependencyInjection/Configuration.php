dd0cbf9ca84e8b02c152a616a69f02f2f0bd4d41
artgris <a.gribet@gmail.com>
2019-10-18 15:44:32 +0200

    first commit

---
+++ src/DependencyInjection/Configuration.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace Artgris\Bundle\EasyAdminCommandsBundle\DependencyInjection;
+
+use Symfony\Component\Config\Definition\Builder\TreeBuilder;
+use Symfony\Component\Config\Definition\ConfigurationInterface;
+
+class Configuration implements ConfigurationInterface
+{
+    /**
+     * Generates the configuration tree builder.
+     *
+     * @return \Symfony\Component\Config\Definition\Builder\TreeBuilder The tree builder
+     */
+    public function getConfigTreeBuilder()
+    {
+        $treeBuilder = new TreeBuilder('artgris_easy_admin_commands');
+        $treeBuilder->getRootNode()
+            ->children()
+                ->scalarNode('dir')
+                    ->defaultValue('%kernel.project_dir%/config/packages/easy_admin/entities/')
+                ->end()
+                ->arrayNode('namespaces')
+                    ->defaultValue(['App\Entity'])
+                    ->scalarPrototype()
+                    ->end()
+                ->end()
+                ->arrayNode('types')
+                    ->normalizeKeys(false)
+                    ->defaultValue([])
+                    ->prototype('variable')
+                    ->end()
+                ->end()
+                ->arrayNode('regex')
+                    ->normalizeKeys(false)
+                    ->defaultValue([])
+                    ->prototype('variable')
+                    ->end()
+                ->end()
+                ->arrayNode('list')
+                    ->addDefaultsIfNotSet()
+                    ->children()
+                        ->arrayNode('exclude')
+                            ->scalarPrototype()
+                            ->end()
+                        ->end()
+                        ->arrayNode('include')
+                            ->scalarPrototype()
+                            ->end()
+                        ->end()
+                    ->end()
+                ->end()
+            ->arrayNode('form')
+                    ->addDefaultsIfNotSet()
+                    ->children()
+                        ->arrayNode('exclude')
+                            ->scalarPrototype()
+                            ->end()
+                        ->end()
+                        ->arrayNode('include')
+                            ->scalarPrototype()
+                            ->end()
+                        ->end()
+                    ->end()
+                ->end()
+            ->end();
+
+        return $treeBuilder;
+    }
+}
